16072acb6b648c75480ceae7b26544b7cd982d35
johnathon <jenson.j.reggentin@hochschule-stralsund.de>
2025-02-16 13:08:20 +0100

    working view change

--- app/CalendarPage/index.tsx
+++ app/CalendarPage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, Button } from "react-native";
 import { useRouter } from "expo-router";
 import useAuthStore from "../stores/authStore";
@@ -9,7 +9,10 @@ import { Calendar } from "react-native-big-calendar";
 const CalendarPage = () => {
     const { setAuthenticated } = useAuthStore();
     const router = useRouter();
-    const [loading, setLoading] = React.useState(true);
+    const [loading, setLoading] = useState(true);
+    const [viewMode, setViewMode] = useState<
+        "day" | "3days" | "week" | "month"
+    >("week"); // State variable to store the selected view mode
 
     React.useEffect(() => {
         // Simulate fetching data
@@ -21,12 +24,18 @@ const CalendarPage = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Welcome to the Calendar Page!</Text>
-            <View style={{ height: "40%", width: "30%" }}>
+            <View style={{ flexDirection: "row", marginBottom: 10 }}>
+                <Button title="Day" onPress={() => setViewMode("day")} />
+                <Button title="3 Days" onPress={() => setViewMode("3days")} />
+                <Button title="Week" onPress={() => setViewMode("week")} />
+                <Button title="Month" onPress={() => setViewMode("month")} />
+            </View>
+            <View style={{ height: "60%", width: "30%" }}>
                 <Calendar
                     events={[
                         {
                             title: "Meeting",
-                            start: new Date(2025, 1, 16, 1, 0),
+                            start: new Date(2025, 1, 16, 10, 0),
                             end: new Date(2025, 1, 16, 10, 45),
                             backgroundColor: "green",
                         },
@@ -37,11 +46,11 @@ const CalendarPage = () => {
                             backgroundColor: "orange",
                         },
                     ]}
-                    mode="week" // Set the view to week
-                    scrollOffsetMinutes={300} // Start at 5:00 AM (300 minutes from midnight)
                     height={800} // Adjust the height here if needed
+                    mode={viewMode} // Set the view mode based on the selected value
+                    scrollOffsetMinutes={300} // Start at 5:00 AM (300 minutes from midnight)
                     eventCellStyle={(event) => ({
-                        backgroundColor: event.backgroundColor || "orange", // Default color is purple
+                        backgroundColor: event.backgroundColor || "orange", // Default color is orange
                         borderRadius: 5,
                         padding: 5,
                     })}
